ba44197b26021d7223cc1c21f128ece238e86251
Eleazar Fernando Braga <bragafer@usp.br>
2019-02-06 01:13:56 -0200

    Coloquei as habilidades no RobotCode

--- Assets/Anima2D/Examples/Robot/Scripts/Habilities.cs
+++ Assets/Anima2D/Examples/Robot/Scripts/Habilities.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class Habilities : MonoBehaviour
 {
 	private Animator anima;
-	public 	LayerMask m_LayerMask;
 	public Transform hand;
 	public Transform chave;
 	public Transform LaserPosition;
 	public Transform target;
 	public GameObject LaserBeam;
-	private bool m_Started = false;
+    public LayerMask my_mask;
+    public Rigidbody my_rig;
     // Start is called before the first frame update
     void Start()
     {
-    	anima = GetComponent<Animator>();   
-    	m_Started = true;
+    	anima = GetComponent<Animator>();
+        my_rig = GetComponent<Rigidbody>();   
     }
 
     // Update is called once per frame
@@ -24,10 +24,13 @@ public class Habilities : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.Alpha1)){
         	anima.SetBool("Attack", true);
+            my_rig.velocity = new Vector3(0,0,0);
         }else if(Input.GetKey(KeyCode.Alpha2)){
         	anima.SetBool("Heal", true);
+            my_rig.velocity = new Vector3(0,0,0);
         }else if(Input.GetKey(KeyCode.Alpha3)){
         	anima.SetBool("Laser", true);
+            my_rig.velocity = new Vector3(0,0,0);
             if(target.position.x > transform.position.x){
                 this.transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
             }else{
@@ -40,58 +43,35 @@ public class Habilities : MonoBehaviour
         }
     }
 
-    void OnDrawGizmos(){
-    	Gizmos.color = Color.red;
-        //Check that it is being run in Play Mode, so it doesn't try to draw this in Editor mode
-        if (m_Started){
-            //Draw a cube where the OverlapBox is (positioned where your GameObject is as well as a size)
-            Gizmos.DrawWireCube(chave.position, transform.localScale/2);
-        }
-    }
-
     public void ApplyDamage(){
-    	LayerMask mask = LayerMask.GetMask("Enemy");
+    	LayerMask mask = (1 << this.gameObject.layer);
+        mask |= (1 << 11);
+        mask = ~mask;
     	Collider[] hitColliders = Physics.OverlapBox(hand.position, transform.localScale/2, Quaternion.identity, mask);
-        int i = 0;
 
-        while (i < hitColliders.Length)
-        {
-            //Output all of the collider names
-            Debug.Log("Hit : " + hitColliders[i].name + i);
-            //Increase the number of Colliders in the array
-            i++;
-        }
         if(hitColliders.Length > 0){
+            print(hitColliders[0].name);
         	hitColliders[0].GetComponent<Alive>().lifes -= 1.0f;
             hitColliders[0].transform.GetChild(9).GetComponent<Animator>().SetTrigger("Hitted");
         }
-    	print("Ataquei");
     }
 
     public void ApplyHeal(){
-    	LayerMask mask = LayerMask.GetMask("Friend");
+    	LayerMask mask = 1 << this.gameObject.layer;
     	Collider[] hitColliders = Physics.OverlapBox(chave.position, transform.localScale/2, Quaternion.identity, mask);
-        int i = 0;
 
-        while (i < hitColliders.Length)
-        {
-            //Output all of the collider names
-            Debug.Log("Hit : " + hitColliders[i].name + i);
-            //Increase the number of Colliders in the array
-            i++;
-        }
         if(hitColliders.Length > 0){
         	hitColliders[0].GetComponent<Alive>().lifes += 1.0f;
             hitColliders[0].transform.GetChild(9).GetComponent<Animator>().SetTrigger("Healed");
         }
-
-    	print("Curei");
     }
 
     public void ApplyLaserBeam(){
     	GameObject laser = Instantiate(LaserBeam, LaserPosition.position, Quaternion.identity) as GameObject;
     	laser.transform.localScale = transform.localScale;
     	laser.GetComponent<Laser>().target = target;
-
+        laser.GetComponent<Laser>().mask = (1 << this.gameObject.layer);
+        laser.GetComponent<Laser>().mask |= (1 << 11);
+        laser.GetComponent<Laser>().mask = ~laser.GetComponent<Laser>().mask;
     }
 }
