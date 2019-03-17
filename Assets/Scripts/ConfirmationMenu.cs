1a32e2bf465fec0e8550add21a68a89f1d730d91
Óliver Becker <oliverbecker@usp.br>
2019-03-17 02:19:31 +0000

    Tela de confirmação genérica
    Criada a tela de confirmação customizável para ser
    utilizada sempre que uma ação a ser realizada por
    um botão necessite confirmar ciência.
    O script deve ser anexado a um botão e pode ser
    customizado pelo inspector. Ele utilizará os
    eventos configurados no próprio botão ao qual ele
    está acoplado caso o usuário clique no botão de
    confirmar.
    A ideia desse script pode ser expandida para a
    customização da tela de preview das fases =)

--- Assets/Scripts/ConfirmationMenu.cs
+++ Assets/Scripts/ConfirmationMenu.cs
@@ -6,32 +6,40 @@ using UnityEngine.Events;
 
 public class ConfirmationMenu : MonoBehaviour {
 
-	public GameObject prefab;
+	private static GameObject prefab;	// Referência ao prefab da Tela de Confirmação (TdC)
 
-	// >> Configurations
+	// >> Configurations of the screen
 	[Range(0.0f, 1.0f)]
-	public float anchorMin = 0.2f;
+	public float anchorMin = 0.2f;	// Ajusta os valores X e Y min da âncora da TdC
 	[Range(0.0f, 1.0f)]
-	public float anchorMax = 0.8f;
-	public Sprite backgroundSprite;
-	public Color backgroundColor = Color.grey;
+	public float anchorMax = 0.8f;	// Ajusta os valores X e Y max da âncora da TdC
+	public Sprite backgroundSprite;	// O sprite a ser adicionado à imagem de fundo
+	public Color backgroundColor = Color.grey;	// Cor da imagem de fundo
 	[TextArea]
-	public string titleText = "Title";
+	public string titleText = "Title";	// Título mostrado na TdC
 	[TextArea]
-	public string bodyText = "<Message>";
-	public Button.ButtonClickedEvent buttonConfirmation;
+	public string bodyText = "<Message>";	// Mensagem explicando sobre a confirmação
 	// Configurations <<
 
-	private Transform canvas;
-	private GameObject instance = null;
+	private Button firstButton;		// Referência ao botão qhe este script está acoplado
+	private Button.ButtonClickedEvent confirmationEvent;	// Evento chamado pelo botão da TdC
+	private Transform canvas;		// Reference to the canvas of the scene
+
+	private GameObject instance = null;	// Referência à INSTÂNCIA da TdC
+	
+	void Awake() {
+		if (prefab == null) {	// Caso o prefab ainda não tenha sido carregado
+			prefab = (GameObject) Resources.Load("ConfirmationScreen");	// Recupera o recurso da TdC
+			if (prefab == null)	// Caso não encontre o prefab, indica erro
+				Debug.Log("ERROR!! The resource ConfirmationScreen could not be loaded!");
+		}
+	}
 
 	// Start is called before the first frame update
 	void Start() {
-		canvas = GameObject.Find("Canvas").transform;
-		ConfigAnchors();
-		ConfigBg();
-		ConfigTexts();
-		ConfigConfirmation();
+		canvas = GameObject.Find("Canvas").transform;	// Get the canvas
+		
+		ConfigButton();
 	}
 
 	public void CreateConfirmationMenu() {
@@ -39,54 +47,93 @@ public class ConfirmationMenu : MonoBehaviour {
 			instance = Instantiate(prefab, canvas);
 		else
 			instance.SetActive(true);
+
+		Configurations();
 	}
 
+	public void Configurations() {
+		ConfigAnchors();
+		ConfigBg();
+		ConfigTexts();
+		ConfigConfirmation();
+	}
+
+	/// <summary>
+	/// Configura o botão ao qual este script está acoplado, removendo o evento que havia (salva em "confirmationEvent")
+	/// e criando um novo para acionar a TdC
+	/// </summary>
+	private void ConfigButton() {
+		firstButton = GetComponent<Button>();
+		if (firstButton == null) {	// Caso não haja botões acoplados, retorna erro
+			Debug.Log("Error! This Game Object must have an Button attached to it!");
+			return;
+		}
+
+		confirmationEvent = firstButton.onClick;	// Salva o evento que estava no botão para ser adicionado à TdC
+
+		Button.ButtonClickedEvent newButtonEvent = new Button.ButtonClickedEvent();
+		newButtonEvent.AddListener(CreateConfirmationMenu);	// Cria um novo evento para chamar a TdC
+
+		firstButton.onClick = newButtonEvent;		// Now the button will call the confirmation menu =D
+	}
+
+	/// <summary>
+	/// Configura as âncoras da tela
+	/// </summary>
 	private void ConfigAnchors() {
 		if (anchorMin >= anchorMax) {
 			Debug.Log("ANCHOR PROBLEM! Anchor Min must be lower than Anchor Max.");
 			return;
 		}
-		RectTransform tranf = prefab.GetComponent<RectTransform>();
+		RectTransform tranf = instance.GetComponent<RectTransform>();
 		tranf.anchorMin = new Vector2(anchorMin, anchorMin);
 		tranf.anchorMax = new Vector2(anchorMax, anchorMax);
 	}
 
+	/// <summary>
+	/// Configura a imagem de fundo
+	/// </summary>
 	private void ConfigBg() {
-		Image bg = prefab.GetComponent<Image>();
+		Image bg = instance.GetComponent<Image>();
 		bg.color = backgroundColor;
 		if (backgroundSprite != null)
 			bg.sprite = backgroundSprite;
 	}
 
+	/// <summary>
+	/// Configura os textos da TdC
+	/// </summary>
 	private void ConfigTexts() {
-		Text title = null;
-		Text message = null;
+		Text title = null;		// Referência do titulo
+		Text message = null;	// Referência da mensagem
 
-		foreach(Transform trans in prefab.transform) {
-			Text aux = trans.GetComponent<Text>();
-			if (aux != null && aux.name == "Título")
+		foreach(Transform trans in instance.transform) {	// Percorre todos os filhos diretos da TdC
+			Text aux = trans.GetComponent<Text>();			// Pega o componente de texto
+			if (aux != null && aux.name == "Título")		// Se o componente existir e for o título, salva a referência
 				title = aux;
-			else if (aux != null && aux.name == "Mensagem")
+			else if (aux != null && aux.name == "Mensagem")	// O mesmo para a mensagem...
 				message = aux;
 		}
 		
-		if (title == null || message == null) {
+		if (title == null || message == null) {		// Caso não encontre alguma referência, retorna erro
 			Debug.Log("ERRO!!!! Título e corpo de mensagem não encontrados");
 			return;
 		}
 
-		title.text = titleText;
+		title.text = titleText;		// Coloca os textos no lugar
 		message.text = bodyText;
 	}
 
-	// Changes the OnClick action of the button confirm to the action in "buttonConfirmation"
-	// Next step is to get the event from the button this script is atached to and set it to the confirmation (also,
-	// set the first button event to open the confirm interface)
+	/// <summary>
+	/// Configura o botão de confirmação da TdC
+	/// </summary>
 	private void ConfigConfirmation() {
-		foreach(Transform trans in prefab.transform) {
-			Button button = trans.GetComponent<Button>();
-			if (button != null && button.name == "Confirmar")
-				button.onClick =  buttonConfirmation;
+		foreach(Transform trans in instance.transform) {	// Percorre todos os filhos diretos da TdC
+			Button button = trans.GetComponent<Button>();	// Pega o componente de botão
+			if (button != null && button.name == "Confirmar") {	// Caso seja o botão de confirmação
+				button.onClick =  confirmationEvent;		// Coloca os antigos eventos do botão inicial
+				button.onClick.AddListener(delegate {instance.SetActive(false); });	// e desativa a TdC quando confirmado
+			}
 		}
 	}
 }
