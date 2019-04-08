7151d49293c7a9b0d01bb5dbc161f8a0d50ec31c
Gabriel Simmel Nascimento <galbrato@gmail.com>
2019-04-08 18:01:48 -0300

    Criei cena amais de batalha e a batalha 7 apra dar feedback

---
+++ Assets/Link.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Link : MonoBehaviour
+{
+    public void _OpenFeedback() {
+        Application.OpenURL("https://forms.gle/GzGcmtV7Q5VjaGA88");
+    }
+}
