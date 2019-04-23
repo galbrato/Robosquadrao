bdcf9f1fb32dd221d6a3d369d1345e67d3b91aec
Eleazar <bragafer@usp.br>
2019-04-23 00:54:47 -0300

    Tela de informação na programação e documentacao dos botoes atualizados!

--- Assets/Scripts/DocFuction.cs
+++ Assets/Scripts/DocFuction.cs
@@ -6,11 +6,12 @@ using UnityEngine.UI;
 public class DocFuction : MonoBehaviour
 {
     // Start is called before the first frame update
-
+    public string funcao;
     public string parametro;
     public string saida;
     [TextArea]
     public string contexto;
+    public Text ref_funcao;
     public Text ref_parametro;
     public Text ref_saida;
     public Text ref_contexto;
@@ -19,6 +20,7 @@ public class DocFuction : MonoBehaviour
         ref_parametro.text = parametro;
         ref_saida.text = saida;
         ref_contexto.text = contexto;
+        ref_funcao.text = funcao;
 
         inter.SetActive(true);
     }
