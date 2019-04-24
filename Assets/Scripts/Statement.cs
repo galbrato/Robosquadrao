8d73f52f1518934b89e3df30d757c365ea252a43
Óliver S Becker <oliverbecker@usp.br>
2019-04-24 15:41:22 -0300

    Criando novas fases!
    Criando e testando elas. Falta testar a 8 e criar
    a 9, mas deu ruim no código pra passar a fase 8.

--- Assets/Scripts/Statement.cs
+++ Assets/Scripts/Statement.cs
@@ -1044,8 +1044,8 @@ public class Se : Statement {
         Parametros[0].Execute(Robot);
         //Verificando se retorno do parametro foi passado
         if (Robot.Retorno == null) {
-            Debug.LogError("Retorno Nulo");
-            //return false;
+            //Debug.LogError("Retorno Nulo");
+            return false;
         }
         //Verificando se o retorno é do tipo correto
         if (Robot.Retorno.type != Tipo.Booleano) {
@@ -1297,8 +1297,8 @@ public class Nao : Statement {
             return false;
         }
         Parametros[0].Execute(Robot);
-        if (Robot.Retorno.type != ParametrosTipos[0]) {
-            Debug.LogError("ERRO! Tipo retornado diferente de Boleano");
+        if (Robot.Retorno == null || Robot.Retorno.type != ParametrosTipos[0]) {
+            //Debug.LogError("ERRO! Tipo retornado diferente de Boleano");
             return false;
         }
         VarBooleano r = (VarBooleano)Robot.Retorno;
