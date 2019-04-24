044163b58cfd68b021612880468d7929d263fc3e
Óliver S Becker <oliverbecker@usp.br>
2019-04-23 22:12:10 -0300

    Começo da tentativa de criação do tutorial
    Criação do tutorial congelada para finalizar tudo
    que falta para amanhã.

---
+++ Assets/Scripts/Tutorial/ButtonTutorial.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonTutorial : MonoBehaviour {
+
+	public Button botao;
+
+	public GameObject fundo;
+	public Sprite spriteBotao;
+
+	private GameObject canvas;
+	private GameObject instanciaFundo;
+
+	// Start is called before the first frame update
+	void Start() {
+		if (botao == null)
+			Debug.LogWarning("ERRO! Referência ao botão do tutorial inexistente!");
+
+		
+		canvas = FindObjectOfType<Canvas> ().gameObject;
+
+		AtivaTutorial();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		
+	}
+
+	public void CompletaTutorial() {
+		Destroy(instanciaFundo);
+	}
+
+	public void AtivaTutorial() {
+		instanciaFundo = Instantiate(fundo, canvas.transform);
+		GameObject instanciaBotao = Instantiate(botao.gameObject, instanciaFundo.transform);
+
+		if (instanciaBotao.GetComponent<ButtonTutorial>() != null)
+			Destroy(instanciaBotao.GetComponent<ButtonTutorial> ());
+
+		instanciaBotao.GetComponent<Image> ().sprite = spriteBotao;
+		instanciaBotao.GetComponent<Image> ().type = Image.Type.Simple;
+		instanciaBotao.GetComponent<Button> ().onClick.AddListener(CompletaTutorial);
+	}
+}
